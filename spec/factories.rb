f05df0fb39590b9a3e5b64cbbb1303819eb2c97f
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2022-07-15 10:05:52 +0100

    add appointment creation tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,22 @@
 
 FactoryBot.define do
   factory(:user) do
-    email { 'abc@example.com' }
+    email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
-    last_name { 'doe' } 
-    first_name { 'john' }
+    last_name { Faker::Name.first_name } 
+    first_name { Faker::Name.last_name }
     role { 'patient' }
+  end
+
+  factory(:doctor) do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+  end
+
+  factory(:appointment) do
+    date { Faker::Time.forward(days: 5, period: :morning) }
+    user
+    doctor
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe User, :type => :model do
   describe "object creation by the user model" do
     context "with valid attributes" do 
       let(:user) {create(:user)}
-      it "creates a valid link" do
+      it "creates a valid user" do
         expect(user).to be_valid
         expect(User.count).to eq(1)
       end
@@ -11,7 +11,7 @@ RSpec.describe User, :type => :model do
 
     context "with invalid attributes" do 
       let(:user) {build(:user, email: "123hfjd")}
-      it "does not create a valid link" do
+      it "does not create a valid user" do
         expect(user).to be_invalid
         expect(user.errors.messages[:email]).to  eq(["is invalid"])
         expect(User.count).to eq(0)
